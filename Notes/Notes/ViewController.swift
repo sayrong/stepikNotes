6dea83c9437e91d5bf4efe89ee5dcca5892d52e0
Dmitriy <dskorohodov@boardmaps.com>
2019-07-09 15:41:37 +0300

    scrollview and elements

--- Notes/Notes/ViewController.swift
+++ Notes/Notes/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
+    @IBOutlet weak var colorRect1: UIView!
+    @IBOutlet weak var colorRect2: UIView!
+    @IBOutlet weak var colorRect3: UIView!
+    @IBOutlet weak var colorRect4: UIView!
+    @IBOutlet weak var noteText: UITextView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var scrollViewContent: UIView!
+    
+    
+    func adjustTextHeight(textView: UITextView) {
+        textView.translatesAutoresizingMaskIntoConstraints = true
+        textView.sizeToFit()
+    }
+    
+    override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-        
+        //adjustTextHeight(textView: noteText)
         #if TESTQA
             let welcomeMessage = UILabel(frame: CGRect(x: 5, y: 50, width: 200, height: 21))
             welcomeMessage.textColor = .black
@@ -21,7 +36,15 @@ class ViewController: UIViewController {
             welcomeMessage.textAlignment = .center
             super.view.addSubview(welcomeMessage)
         #endif
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
         
+        colorRect1.layer.borderWidth = 1
+        colorRect1.layer.borderColor = UIColor.black.cgColor
+        colorRect2.layer.borderWidth = 1
+        colorRect2.layer.borderColor = UIColor.black.cgColor
+        colorRect3.layer.borderWidth = 1
+        colorRect3.layer.borderColor = UIColor.black.cgColor
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -29,6 +52,15 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+    @IBAction func test(_ sender: Any) {
+        adjustTextHeight(textView: noteText)
+    }
+    
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
 }
 
